7d9f7394e1dbbdec8c4bacf6712afbb6caa03205
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-25 17:09:40 +0100

    Bonus + Click Option Done

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -4,7 +4,25 @@ createApp({
     data(){
         return{
             message : "Hello World!",
-            sourceImg : "https://i.picsum.photos/id/486/300/300.jpg?hmac=cn9aLv_SbYBBugoObhGsWSZINx1MLBEM2inEFST1Ixo"
+            sourceImg : "https://unsplash.it/600/600?image",
+            filter : "filter",
+            color : ""
+        }
+    },
+    methods: {
+        onClickImg: function(){
+            if (this.filter === "filter"){
+                this.filter = "filter2";
+            } else{
+                this.filter = "filter";
+            };
+        },
+        onClickText: function(){
+            if (this.color === "rosso"){
+                this.color = "verde"
+            } else{
+                this.color = "rosso"
+            }
         }
     }
-}).mount("#app")
+}).mount("#app");
